ddf1ba274acd3754ad274ad1d413abb9e0804047
Shikha <shikha@onmobile.com>
2018-05-15 09:43:39 +0530

    '890commit'

--- angular/directive/Main_directive.js
+++ angular/directive/Main_directive.js
@@ -1,44 +1,35 @@
 myApp.directive("myCharacters",function(){
-
     return {
         restrict:"E",
         templateUrl:"./views/characters.html",
         controller : function($scope){
-            var index=$scope.item.url.lastIndexOf('/');
-            $scope.id=$scope.item.url.substring(index+1);
-            if($scope.item.url.search('characters')>-1){
-                if($scope.item.aliases[0].length>1 && !$scope.item.name){
-                    var aliases=$scope.item.aliases[0];
-                    var name=$scope.item.name;
-                    $scope.item.name=aliases;
                 }
             }
         }
     }
 });
-
 myApp.directive("myBooks",function(){
-
     return {
         restrict:"E",
         templateUrl:"./views/books.html",
         controller : function($scope){
-            var index=$scope.item.url.lastIndexOf('/');
-            $scope.id=$scope.item.url.substring(index+1);
-            // console.log($scope.item.name);
         }
     }
 });
-
 myApp.directive("myHouses",function(){
 
     return {
         restrict:"E",
         templateUrl:"./views/houses.html",
-        controller : function($scope){
-            var index=$scope.item.url.lastIndexOf('/');
-            $scope.id=$scope.item.url.substring(index+1);
-            // console.log($scope.item.name);
+        controller : function($scope){    
         }
     }
+});
+myApp.directive("myAllDetails",function(){
+    return {
+        restrict:"E",
+        templateUrl:"./views/main-page.html",
+        controller : function($scope){
+                }
+            }
 });
